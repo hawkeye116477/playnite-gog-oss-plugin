hawkeye116477/playnite-gog-oss-plugin

commit 3edf475d62ff390f9d446d0154eaba8767997f6a

Fix launching Dosbox games

src/GogOssGameController.cs
@@ -410,7 +410,13 @@ namespace GogOssLibraryNS
             {
                 var task = GogOssLibrary.GetPlayTasks(Game.GameId, Game.InstallDirectory);
                 var gameExe = task[0].Path;
-                if (File.Exists(gameExe))
+                var workingDir = task[0].WorkingDir;
+                var gameExeFullPath = gameExe;
+                if (!workingDir.IsNullOrEmpty())
+                {
+                    gameExeFullPath = Path.Combine(workingDir, gameExe);
+                }
+                if (File.Exists(gameExeFullPath))
                 {
                     var playArgs = new List<string>();
                     var gameSettings = GogOssGameSettingsView.LoadGameSettings(Game.GameId);
@@ -422,10 +428,19 @@ namespace GogOssLibraryNS
                     {
                         gameExe = gameSettings.OverrideExe;
                     }
-                    var cmd = Cli.Wrap(gameExe)
+                    if (!task[0].Arguments.IsNullOrEmpty())
+                    {
+                        var providedArgs = task[0].Arguments.Replace("\"", "").Replace("'", "").Split().ToList();
+                        playArgs.AddRange(providedArgs);
+                    }
+                    var cmd = Cli.Wrap(gameExeFullPath)
                                  .WithArguments(playArgs)
                                  .AddCommandToLog()
                                  .WithValidation(CommandResultValidation.None);
+                    if (!workingDir.IsNullOrEmpty())
+                    {
+                        cmd = cmd.WithWorkingDirectory(workingDir);
+                    }
                     await foreach (var cmdEvent in cmd.ListenAsync())
                     {
                         switch (cmdEvent)
@@ -439,6 +454,14 @@ namespace GogOssLibraryNS
                         }
                     }
                 }
+                else
+                {
+                    InvokeOnStopped(new GameStoppedEventArgs());
+                }
+            }
+            else
+            {
+                InvokeOnStopped(new GameStoppedEventArgs());
             }
         }
 

commit e17b11f11121f1585c9c6781aff71c2ce5c26df5

Fix recent regression
Fix from https://github.com/hawkeye116477/playnite-gog-oss-plugin/commit/3edf475d62ff390f9d446d0154eaba8767997f6a caused problems with launching other games.

src/GogOssGameController.cs
@@ -420,6 +420,15 @@ namespace GogOssLibraryNS
                 {
                     var playArgs = new List<string>();
                     var gameSettings = GogOssGameSettingsView.LoadGameSettings(Game.GameId);
+                    if (!task[0].Arguments.IsNullOrEmpty())
+                    {
+                        var providedArgs = Helpers.SplitArguments(task[0].Arguments);
+                        playArgs.AddRange(providedArgs);
+                        if (workingDir.IsNullOrEmpty())
+                        {
+                            workingDir = Path.GetDirectoryName(gameExeFullPath);
+                        }
+                    }
                     if (gameSettings.StartupArguments?.Any() == true)
                     {
                         playArgs.AddRange(gameSettings.StartupArguments);
@@ -427,11 +436,6 @@ namespace GogOssLibraryNS
                     if (!gameSettings.OverrideExe.IsNullOrEmpty())
                     {
                         gameExe = gameSettings.OverrideExe;
-                    }
-                    if (!task[0].Arguments.IsNullOrEmpty())
-                    {
-                        var providedArgs = task[0].Arguments.Replace("\"", "").Replace("'", "").Split().ToList();
-                        playArgs.AddRange(providedArgs);
                     }
                     var cmd = Cli.Wrap(gameExeFullPath)
                                  .WithArguments(playArgs)

src/Helpers.cs
@@ -148,5 +148,31 @@ namespace GogOssLibraryNS
             // Uri's also escape some chars, co convert back to unescaped format
             return Uri.UnescapeDataString(relativeUri.ToString().Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar));
         }
+
+        /// <summary>
+        /// Found at https://stackoverflow.com/a/2132004
+        /// </summary>
+        public static string[] SplitArguments(string commandLine)
+        {
+            var parmChars = commandLine.ToCharArray();
+            var inSingleQuote = false;
+            var inDoubleQuote = false;
+            for (var index = 0; index < parmChars.Length; index++)
+            {
+                if (parmChars[index] == '"' && !inSingleQuote)
+                {
+                    inDoubleQuote = !inDoubleQuote;
+                    parmChars[index] = '\n';
+                }
+                if (parmChars[index] == '\'' && !inDoubleQuote)
+                {
+                    inSingleQuote = !inSingleQuote;
+                    parmChars[index] = '\n';
+                }
+                if (!inSingleQuote && !inDoubleQuote && parmChars[index] == ' ')
+                    parmChars[index] = '\n';
+            }
+            return new string(parmChars).Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        }
     }
 }