81e28c5c953c79f24974e4b7348e32a5fb302716
hawkeye116477 <hawkeye116477@gmail.com>
2024-10-27 19:32:48 +0100

    Add small fixes

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GogOssLibraryNS")]
-[assembly: AssemblyCopyright("Copyright © 2023-2024 hawkeye116477\n Copyright © 2020 Josef Nemec")]
+[assembly: AssemblyCopyright("Copyright © 2024 hawkeye116477\n Copyright © 2020 Josef Nemec")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
