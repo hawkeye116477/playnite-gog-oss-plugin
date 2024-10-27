05214c0d2469f3523841bf89da3e032a62bc83cb
hawkeye116477 <hawkeye116477@gmail.com>
2024-10-27 19:28:03 +0100

    Cleanup script

--- make_scripts/download_translations.py
+++ make_scripts/download_translations.py
@@ -14,11 +14,10 @@ src_path = pj(main_path, "src")
 
 def request(url):
     resp = requests.get(url, timeout = 30)
-    if (resp.status_code == 200):
+    if resp.status_code == 200:
         return resp.content
-    else:
-        print("ERROR: " + str(resp.status_code) + " " + resp.reason)
-        return None
+    print("ERROR: " + str(resp.status_code) + " " + resp.reason)
+    return None
 
 class FirstCrowdinClient(CrowdinClient):
     TOKEN = os.getenv("crowdin_token")
@@ -30,7 +29,7 @@ status = ''
 while status != 'finished':
     info = client.translations.check_project_build_status(build['id'])
     status = info['data']['status']
-    if (status != 'finished'):
+    if status != 'finished':
         print(f"Build progress: {info['data']['progress']}")
 download_info = client.translations.download_project_translations(build['id'])
 url = download_info['data']['url']
@@ -43,7 +42,7 @@ with tempfile.TemporaryDirectory() as tmpdirname:
 
     with ZipFile(pj(tmpdirname, "temp.zip"), 'r') as zObject:
         zObject.extractall(pj(tmpdirname))
-    
+
     os.remove(pj(tmpdirname, "temp.zip"))
     xmlns = "http://schemas.microsoft.com/winfx/2006/xaml/presentation"
     xmlns_x = "http://schemas.microsoft.com/winfx/2006/xaml"
@@ -59,7 +58,7 @@ with tempfile.TemporaryDirectory() as tmpdirname:
         for line in legendary_loc_keys_content:
             if line := line.strip():
                 legendary_loc_keys[line] = ""
-    
+
     legendary_loc_path = pj(tmpdirname, "src", "Localization")
     for filename in os.listdir(legendary_loc_path):
         path = os.path.join(legendary_loc_path, filename)
@@ -70,10 +69,10 @@ with tempfile.TemporaryDirectory() as tmpdirname:
                 shutil.copy(path, pj(src_path, "Localization"))
             continue
         legendary_loc = ET.parse(pj(legendary_loc_path, filename))
-    
+
         xml_root = ET.Element("ResourceDictionary", nsmap=NSMAP)
         xml_doc = ET.ElementTree(xml_root)
-    
+
         for child in legendary_loc.getroot():
             key = child.get(ET.QName(xmlns_x, "Key"))
             if key in legendary_loc_keys:
@@ -85,8 +84,8 @@ with tempfile.TemporaryDirectory() as tmpdirname:
                 new_key.set(ET.QName(xmlns_x, "Key"), key)
                 new_key.text = key_text.replace("Legendary", "GOG OSS")
                 xml_root.append(new_key)
-    
+ 
         ET.indent(xml_doc, level=0)
-    
+  
         with open(pj(src_path, "Localization", filename), "w", encoding="utf-8") as i18n_file:
             i18n_file.write(ET.tostring(xml_doc, encoding="utf-8", xml_declaration=True, pretty_print=True).decode())
