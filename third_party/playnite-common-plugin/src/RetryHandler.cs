645be12eb57d381871ba5f80bda62d4a011cfc41
hawkeye116477 <hawkeye116477@gmail.com>
2025-12-20 23:32:11 +0100

    Tidy up - part 2

--- third_party/playnite-common-plugin/src/RetryHandler.cs
+++ third_party/playnite-common-plugin/src/RetryHandler.cs
@@ -4,7 +4,7 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace GogOssLibraryNS
+namespace CommonPlugin
 {
     public class RetryHandler : DelegatingHandler
     {
